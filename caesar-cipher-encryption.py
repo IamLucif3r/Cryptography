c812c879b087b3bd1b6f89d62100cf474a501a4b
iamlucif3r <sanmol016@gmail.com>
2021-08-25 14:54:49 +0530

    Automated Git Commit

--- caesar-cipher-encryption.py
+++ caesar-cipher-encryption.py
@@ -8,13 +8,13 @@ def encypt_func(txt, s):
   
         if (char.isupper()):  
             result += chr((ord(char) + s - 64) % 26 + 65)  
-        # encypt_func lowercase characters in plain txt  
+        # encypt_func lowercase characters in plain hetxt  
         else:  
             result += chr((ord(char) + s - 96) % 26 + 97)  
     return result  
 # check the above function  
 txt = "ANMOL SINGH YADAV"  
-s = 2 
+s = 3 
   
 print("Plain txt : " + txt)  
 print("Shift pattern : " + str(s))  
---
+++ caesar-cipher.cpp
@@ -0,0 +1,62 @@
+    #include<bits/stdc++.h>
+    using namespace std;
+   
+    int main() {
+    cout<<"[*] ENTER PLAIN TEXT:\n";
+    char msg[100];
+    cin.getline(msg,100); //take the message as input
+    int i, j, length,choice,key;
+    cout << "[*] ENTER KEY: \t";
+    cin >> key; //take the key as input
+    length = strlen(msg);
+    cout<<"[*] SELECT YOUR OPTION \n 1. ENCRYPTION \n 2. DECRYPTION \n";
+     cin>>choice;
+    if (choice==1) //for encryption
+    {
+        char ch;
+        for(int i = 0; msg[i] != '\0'; ++i) {
+            ch = msg[i];
+            //encrypt for lowercase letter
+            if (ch >= 'a' && ch <= 'z'){
+                ch = ch + key;
+                if (ch > 'z') {
+                ch = ch - 'z' + 'a' - 1;
+                }  
+                msg[i] = ch;
+            }
+            //encrypt for uppercase letter
+            else if (ch >= 'A' && ch <= 'Z'){
+                ch = ch + key;
+                if (ch > 'Z'){
+                ch = ch - 'Z' + 'A' - 1;
+                }
+                msg[i] = ch;
+            }
+        }
+        printf("[*] ENCRYPTED MESSAGE: %s", msg);
+        cout<<endl;
+    }
+    else if (choice == 2) { //for decryption
+        char ch;
+        for(int i = 0; msg[i] != '\0'; ++i) {
+            ch = msg[i];
+            //decrypt for lowercase letter
+            if(ch >= 'a' && ch <= 'z') {
+                ch = ch - key;
+                if(ch < 'a'){
+                ch = ch + 'z' - 'a' + 1;
+                }
+                msg[i] = ch;
+            }
+            //decrypt for uppercase letter
+            else if(ch >= 'A' && ch <= 'Z') {
+                ch = ch - key;
+                if(ch < 'A') {
+                ch = ch + 'Z' - 'A' + 1;
+                }
+                msg[i] = ch;
+            }
+        }
+        cout << "[*] DECRYPTED MESSAGE : " << msg<<endl;
+    }
+    }
